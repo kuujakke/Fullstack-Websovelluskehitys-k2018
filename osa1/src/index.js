fecd4a6bba978f4dddc15ae90f8877045c4ab10e
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-01-18 22:55:41 +0200

    tehtävä 1.3

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
-
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
+    let osat = [osa1, osa2, osa3]
     return (
         [
             <Otsikko kurssi={kurssi}/>,
-            <Sisalto tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} osa1={osa1} osa2={osa2}
-                     osa3={osa3}/>,
-            <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
+            <Sisalto osat={osat}/>,
+            <Yhteensa osat={osat}/>
         ]
     )
 }
@@ -27,25 +32,26 @@ const Otsikko = (props) => {
 }
 
 const Sisalto = (props) => {
-    return (
-        [
-            <Osa osa={props.osa1} tehtavia={props.tehtavia1}/>,
-            <Osa osa={props.osa2} tehtavia={props.tehtavia2}/>,
-            <Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
-        ]
-
-    )
+    let komponentit = []
+    props.osat.forEach(function (osa) {
+        komponentit.push(Osa(osa))
+    })
+    return (komponentit)
 }
 
 const Yhteensa = (props) => {
+    let yhteensa = 0
+    props.osat.forEach(function (osa) {
+        yhteensa += osa.tehtavia
+    })
     return (
-        <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+        <p>yhteensä {yhteensa} tehtävää</p>
     )
 }
 
 const Osa = (props) => {
     return (
-        <p>{props.osa} {props.tehtavia}</p>
+        <p>{props.nimi} {props.tehtavia}</p>
     )
 }
 
