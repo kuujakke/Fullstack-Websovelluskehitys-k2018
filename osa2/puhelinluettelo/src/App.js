936f88c7ccbef437f35fa3d2db61afb1a6b070be
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-01-21 00:00:34 +0200

    Tehtava 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,32 +5,41 @@ class App extends React.Component {
         super(props)
         this.state = {
             persons: [
-                { name: 'Arto Hellas' }
+                {
+                    id: 0,
+                    name: 'Arto Hellas',
+                    number: '0700-123123'
+                }
             ],
-            newName: ''
+            newName: '',
+            newNumber: ''
         }
     }
 
     addNumber = (event) => {
         event.preventDefault()
+        event.stopPropagation()
         const person = {
-            name: this.state.newName
+            id: this.state.persons.length,
+            name: this.state.newName,
+            number: this.state.newNumber
         }
 
         const persons =
-            this.state.persons.find(p => p.name === this.state.newName ) ?
+            this.state.persons.find(p => p.name === this.state.newName | this.state.newName === '') ?
             this.state.persons :
             this.state.persons.concat(person)
 
         this.setState({
             persons,
-            newNote: ''
+            newName: '',
+            newNumber: ''
         })
     }
 
     handleChange = (event) => {
         event.preventDefault()
-        this.setState({ newName: event.target.value })
+        this.setState({ [event.target.title]: event.target.value })
     }
 
     render() {
@@ -41,16 +50,43 @@ class App extends React.Component {
                     <div>nimi:
                         <input
                         value={this.state.newName}
+                        title={"newName"}
                         onChange={this.handleChange}
                         />
                     </div>
-                    <button type="submit">lisää</button>
+                    <div>numero:
+                        <input
+                        value={this.state.newNumber}
+                        title={"newNumber"}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">lisää</button>
+                    </div>
                 </form>
                 <h2>Numerot</h2>
-                ...
+                <table>
+                    <tbody><Persons persons={this.state.persons} /></tbody>
+                </table>
             </div>
         )
     }
+}
+
+const Persons = ({persons}) => {
+    return  persons.length > 0 ?
+            persons.map(p => <Person key={p.id} name={p.name} number={p.number} />) :
+            <tr><td>Ei vielä numeroita</td></tr>
+}
+
+const Person = ({name, number}) => {
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{number}</td>
+        </tr>
+    )
 }
 
 export default App
