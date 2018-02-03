9818dabf10cd58c66c3433d03e6ac6ad0c56e61a
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-02-03 20:28:19 +0200

    Tehtävät 4.4-4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
-    return 1
+const totalLikes = (blogs) => {
+    return blogs.reduce(((sum, blog) => sum += blog.likes), 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.sort((a, b) => a.likes - b.likes)[blogs.length - 1]
 }
 
 module.exports = {
-    dummy
+    totalLikes,
+    favoriteBlog
 }
