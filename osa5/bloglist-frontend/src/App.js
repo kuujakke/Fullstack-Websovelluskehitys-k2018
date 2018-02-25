711f1fcce5379144478999d21eb673b5d21f0538
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-02-25 11:16:36 +0200

    Alkutilanne 5.0

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+class App extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            blogs: [],
+            user: null,
+            username: '',
+            password: '',
+        }
+    }
+
+    componentDidMount () {
+        blogService.getAll().then(blogs =>
+            this.setState({blogs}),
+        )
+    }
+
+    render () {
+        return (
+            <div>
+                <h2>blogs</h2>
+                {this.state.blogs.map(blog =>
+                    <Blog key={blog._id} blog={blog}/>,
+                )}
+            </div>
+        )
+    }
+}
+
+export default App
