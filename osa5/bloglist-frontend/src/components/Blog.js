d418a89a5ec4dc984918cdcabe7d87181a2046ae
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-02-26 21:52:25 +0200

    Tehtävä 5.14

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -3,14 +3,14 @@ import Toggleable from './Toggleable'
 
 const Blog = ({blog, likeHandler, deleteHandler, user}) => (
     <Toggleable buttonLabel={`${blog.title} ${blog.author}`}>
-        <div>
-            <p>{blog.title}</p>
-            <p>{blog.author}</p>
-            <p>{blog.url}</p>
-            <p>{blog.likes} likes
-                <button onClick={likeHandler(blog)}>Like</button>
+        <div className={'blog-details'}>
+            <p className={'blog-title'}>{blog.title}</p>
+            <p className={'blog-author'}>{blog.author}</p>
+            <p className={'blog-url'}>{blog.url}</p>
+            <p className={'blog-likes'}>{blog.likes} likes
+                <button onClick={likeHandler(blog)}
+                        className={'blog-like-button'}>Like</button>
             </p>
-            {console.log(typeof blog.user)}
             {blog.user !== undefined || typeof blog.user !== 'undefined'
                 ? blog.user.username.toString() === user.username.toString()
                     ? <button onClick={deleteHandler(blog)}>Delete</button>
--- osa5/bloglist-frontend/src/components/Toggleable.js
+++ osa5/bloglist-frontend/src/components/Toggleable.js
@@ -18,12 +18,13 @@ class Toggleable extends React.Component {
         const showWhenVisible = {display: this.state.visible ? '' : 'none'}
 
         return (
-            <div>
-                <div style={hideWhenVisible}>
-                    <div onClick={this.toggleVisibility}
-                         className={'toggleable'}>{this.props.buttonLabel}</div>
+            <div className={'toggleable'}>
+                <div style={hideWhenVisible} className={'toggle-header'}>
+                    <div
+                        onClick={this.toggleVisibility}
+                        className={'toggle'}>{this.props.buttonLabel}</div>
                 </div>
-                <div style={showWhenVisible} className={'toggled'}>
+                <div style={showWhenVisible} className={'toggle-content'}>
                     {this.props.children}
                     <button onClick={this.toggleVisibility}>Close</button>
                 </div>
@@ -33,7 +34,7 @@ class Toggleable extends React.Component {
 }
 
 Toggleable.propTypes = {
-    buttonLabel: PropTypes.string.isRequired
+    buttonLabel: PropTypes.string.isRequired,
 }
 
 export default Toggleable
