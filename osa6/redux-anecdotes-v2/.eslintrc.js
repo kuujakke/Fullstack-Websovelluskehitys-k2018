62461139113617ddba76189fcc00c136dbacee94
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-02-27 21:50:14 +0200

    Tehtävä 6.1

---
+++ osa6/redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'jest/globals': true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'experimentalObjectRestSpread': true,
+            'jsx': true,
+        },
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'react',
+        'jest',
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'never',
+        ],
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+    },
+}
