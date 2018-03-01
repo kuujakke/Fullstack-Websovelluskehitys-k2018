ebef63e344372bd8582d63f28fcd434b18133beb
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-03-01 12:19:38 +0200

    Tehtävä 6.12

--- osa6/redux-anecdotes-v2/src/services/anecdotes.js
+++ osa6/redux-anecdotes-v2/src/services/anecdotes.js
@@ -12,4 +12,9 @@ const createNew = async (content) => {
     return response.data
 }
 
-export default { getAll, createNew }
+const putAnecdote = async (anecdote) => {
+    const response = await axios.put(`${baseUrl}/${anecdote.id}`, anecdote)
+    return response.data
+}
+
+export default { getAll, createNew, putAnecdote }
