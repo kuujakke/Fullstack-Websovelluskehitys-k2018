84e2005d95bb92d8060ccda7daa8c4ec46a28b32
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-03-01 14:09:05 +0200

    Tehtävä 6.17

--- osa6/routed-anecdotes/src/App.js
+++ osa6/routed-anecdotes/src/App.js
@@ -14,8 +14,21 @@ const AnecdoteList = ({anecdotes}) => (
         <h2>Anecdotes</h2>
         <ul>
             {anecdotes.map(
-                anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
+                anecdote =>
+                    <li key={anecdote.id}>
+                        <Link to={`/anecdotes/${anecdote.id}`}>
+                            {anecdote.content}</Link>
+                    </li>,
+            )}
         </ul>
+    </div>
+)
+
+const Anecdote = ({anecdote}) => (
+    <div>
+        <h2>{anecdote.content} by {anecdote.author}</h2>
+        <p>Has {anecdote.votes} votes</p>
+        <p>For more info see <a href={anecdote.info}>{anecdote.info}</a></p>
     </div>
 )
 
@@ -156,10 +169,16 @@ class App extends React.Component {
                 <Router>
                     <div>
                         <h1>Software anecdotes</h1>
-                        <Menu />
-                        <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes}/>}/>
-                        <Route path="/about" render={() => <About />}/>
-                        <Route path="/create" render={() => <CreateNew addNew={this.addNew}/>}/>
+                        <Menu/>
+                        <Route exact path="/" render={() => <AnecdoteList
+                            anecdotes={this.state.anecdotes}/>}/>
+                        <Route path="/about" render={() => <About/>}/>
+                        <Route path="/create" render={() => <CreateNew
+                            addNew={this.addNew}/>}/>
+                        <Route exact path="/anecdotes/:id"
+                               render={({match}) => <Anecdote
+                                   anecdote={this.anecdoteById(
+                                       match.params.id)}/>}/>
                         <Footer/>
                     </div>
                 </Router>
